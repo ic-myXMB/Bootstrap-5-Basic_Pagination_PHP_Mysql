24d60ed23ad830a0f7d7cc217a385e72feae2dff
ic-myXMB <106540361+ic-myXMB@users.noreply.github.com>
2022-10-25 06:37:50 -0700

    crumb pretty
    p-2 bg-light rounded-3

--- Tutorial_Create_PHP_Pagination_Select_with_Bootstrap_5_in_PHP_MYSQLI/Upload/index.php
+++ Tutorial_Create_PHP_Pagination_Select_with_Bootstrap_5_in_PHP_MYSQLI/Upload/index.php
@@ -49,7 +49,7 @@ $mysqli = mysqli_connect('localhost', 'db_user', 'db_password', 'db_name');
         ?>
 
         <!-- Breadcrumb -->
-        <ol class="breadcrumb mb-4">
+        <ol class="breadcrumb mb-4 mt-4 p-2 bg-light rounded-3">
              <li class="breadcrumb-item"><?php echo $is_current_dir_icon; ?><a href="../<?php echo $is_current_dir; ?>"><?php echo $is_current_dir; ?></a></li>                   
              <li class="breadcrumb-item active"><?php echo $is_current_page_icon; ?><?php echo $is_current_page; ?></li>
         </ol>			
