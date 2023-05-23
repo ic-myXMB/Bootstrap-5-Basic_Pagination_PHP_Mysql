17a732f3c0e9badf87c4458c8f92c1cee7c8ee10
ic-myXMB <106540361+ic-myXMB@users.noreply.github.com>
2023-05-22 20:07:19 -0700

    bs-fa update external libs linkages ver(s)
    bs alpha-1 to alpha-3 / fa 6.3.0 to 6.4.0

--- Tutorial_Create_Alphabetical_Select_PHP_Pagination_with_Bootstrap_5_in_PHP_MYSQLI/Upload/index.php
+++ Tutorial_Create_Alphabetical_Select_PHP_Pagination_with_Bootstrap_5_in_PHP_MYSQLI/Upload/index.php
@@ -51,9 +51,9 @@
            <title>Tutorial | Create Alphabetic Pagination (Select) in Bootstrap 5 with PHP & MYSQLI</title>
            <!-- CSS Files -->
            <!-- Bootstrap -->  
-           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" />
+           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" />
            <!-- Font Awesome -->
-           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />   
+           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />   
            <style>
                .custom-pagination select {
                      border-radius: 0px;
@@ -184,7 +184,7 @@
            <!-- JQuery -->
            <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> 
            <!-- Bootstrap -->
-           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script> 
+           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script> 
       </body>  
  </html>  
  
